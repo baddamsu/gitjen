2af2d008c771f4b7733e25cd9ac55308173e0a15
baddamsu <30918543+baddamsu@users.noreply.github.com>
2017-10-29 16:34:05 +0530

    Update job1.sh

--- job1.sh
+++ job1.sh
@@ -1,2 +1,13 @@
 !#/bin/bash
-echo " first Job"
+source vmconfig
+if [ $Region -ne "us-east-2" ]
+then 
+echo " Only ohio is allowed for this project "
+exit 1
+fi
+if [ $Type -ne "t2.micro" ]
+then
+echo " Only t2 micro is allowed "
+exit 2
+fi
+
